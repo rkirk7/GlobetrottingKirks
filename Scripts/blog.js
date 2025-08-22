8637a8129d7145e8fc86cb837aaf4e0e0b42b3ed
rkirk7 <129214148+rkirk7@users.noreply.github.com>
2025-08-22 09:29:43 -0400

    Update blog.js

--- Scripts/blog.js
+++ Scripts/blog.js
@@ -1,10 +1,14 @@
 async function initBlog() {
   const posts = [
+    "2025-08-14-panda-monium-close-encounters-in-chengdu-and-wolong-china.md",
+    "2025-06-03-exploring-antarctica-and-the-arctic-how-did-two-floridians-end-up-at-both-polar-regions-in-one-year-and-what-did-they-discover.md",
+    "2025-01-29-antarctica-calls-and-we-must-go.md",
+    "2024-09-12-in-search-of-polar-bears-in-the-arctic.md",
+        "2015-10-03-reunion-magic-returning-to-goudy-and-lane.md",
+      //  "2015-09-28-reunion-magic-yes-you-can-go-home-again.md",
+        "2015-09-16-retirement-milestone-3-months-and-counting.md",
+        "2015-09-14-yes-the-world-needs-another-website-mine.md",
     "2015-08-29-hello-world-2.md",
-    "2015-09-14-yes-the-world-needs-another-website-mine.md",
-    "2015-09-16-retirement-milestone-3-months-and-counting.md",
-  //  "2015-09-28-reunion-magic-yes-you-can-go-home-again.md",
-    "2015-10-03-reunion-magic-returning-to-goudy-and-lane.md"
   ];
 
   const container = document.getElementById("blog-container");
